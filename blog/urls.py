ce2118d5421c607f862a0404a58340dcc805b50b
SMS3384 <akkuntsms8@gmail.com>
2024-12-10 08:32:17 +0500

    base,list,models,urls,views

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blogapp.urls',namespace='blogapp'))
+    # path('',include('blogapp.urls',namespace='blogapp'))
+    path('',include('blogapp.urls',namespace='blog'))
 ]
